94399e4fb0077b262d20823e054a4b4a45558e2b
Nadia Paliuha <nadyapaliuha@gmail.com>
2025-11-21 14:35:20 +0200

    Connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,16 @@ import { UserModule } from './user/user.module';
 import { RestaurantModule } from './restaurant/restaurant.module';
 import { BookingModule } from './booking/booking.module';
 import { TableModule } from './table/table.module';
+import { ormConfig } from '../ormconfig';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, RestaurantModule, BookingModule, TableModule],
+  imports: [
+    TypeOrmModule.forRoot(ormConfig),
+    UserModule,
+    RestaurantModule,
+    BookingModule,
+    TableModule,
+  ],
 })
 export class AppModule {}
