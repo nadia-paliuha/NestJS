ef058444c79b9f63504f05385f4eca633e37462e
Nadia Paliuha <nadyapaliuha@gmail.com>
2025-11-22 15:25:43 +0200

    Added access config for users roles

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
 import { TableService } from './table.service';
 import { Table } from './table.entity';
 
@@ -7,26 +10,36 @@ export class TableController {
   constructor(private readonly tableService: TableService) {}
 
   @Get()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
   getAll(): Promise<Table[]> {
     return this.tableService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
   getOne(@Param('id') id: number): Promise<Table> {
     return this.tableService.findOne(id);
   }
 
   @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
   create(@Body() data: Partial<Table>, @Body('restaurantId') restaurantId: number): Promise<Table> {
     return this.tableService.create(data, restaurantId);
   }
 
   @Put(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
   update(@Param('id') id: number, @Body() data: Partial<Table>): Promise<Table> {
     return this.tableService.update(id, data);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
   delete(@Param('id') id: number): Promise<void> {
     return this.tableService.delete(id);
   }
